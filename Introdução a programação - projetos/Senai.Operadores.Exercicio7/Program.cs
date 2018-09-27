dfb384c0db55b1c7008e52b21390a37905a8b9f9
arielmn22 <arielpaixao10@gmail.com>
2018-09-26 22:27:20 -0300

    Exercício 7

--- Introdução a programação - projetos/Senai.Operadores.Exercicio7/Program.cs
+++ Introdução a programação - projetos/Senai.Operadores.Exercicio7/Program.cs
@@ -6,7 +6,18 @@ namespace Senai.Operadores.Exercicio7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            float dolar, real;
+
+            Console.Clear();
+            Console.WriteLine("Conversor de dólar para real:\n");
+
+            Console.WriteLine("Insira a quantidade de reais a serem convertidos:");
+            real = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o valor do dólar em reais:");
+            dolar = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"R${real} = US${real * dolar}");
         }
     }
 }
