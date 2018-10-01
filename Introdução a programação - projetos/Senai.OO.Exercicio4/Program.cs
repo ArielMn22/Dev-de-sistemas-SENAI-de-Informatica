a9d45c96ba0fbc073db6225355ad0a4366e612d6
Ariel Paixão Dos Santos <33747821839@info.local>
2018-10-01 14:05:20 -0300

    Corrigindo Exercícios 4

--- Introdução a programação - projetos/Senai.OO.Exercicio4/Program.cs
+++ Introdução a programação - projetos/Senai.OO.Exercicio4/Program.cs
@@ -10,31 +10,40 @@ namespace Senai.OO.Exercicio4
             CartaoDeCredito card1 = new CartaoDeCredito();
             CartaoDeCredito card2 = new CartaoDeCredito();
 
+            Console.Clear();
             Console.WriteLine("Cartões de Crédito\n");
             
             #region card1
             Console.WriteLine("Insira o nome do primeiro cartão:");
             card1.nome = Console.ReadLine();
 
-            Console.WriteLine("Insira o número do cartão:");
+            Console.WriteLine("\nInsira o número do cartão:");
             card1.numero = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Insira a data limite de pagamento do cartão de crédito:");
+            Console.WriteLine("\nInsira a data limite de pagamento do cartão de crédito:");
             card1.data_limite = Convert.ToDateTime(Console.ReadLine());
             #endregion
 
             #region card2
-            Console.WriteLine("Insira o nome do primeiro cartão:");
+            Console.WriteLine("\nInsira o nome do segundo cartão:");
             card1.nome = Console.ReadLine();
 
-            Console.WriteLine("Insira o número do cartão:");
+            Console.WriteLine("\nInsira o número do cartão:");
             card1.numero = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Insira a data limite de pagamento do cartão de crédito:");
+            Console.WriteLine("\nInsira a data limite de pagamento do cartão de crédito:");
             card1.data_limite = Convert.ToDateTime(Console.ReadLine());
             #endregion
 
-            //CONTINUAR
+            //Saida
+            Console.Clear();
+            Console.WriteLine($"Nome do cartão 1: {card1.nome}");
+            Console.WriteLine($"Número: {card1.numero}");
+            Console.WriteLine($"Data limite de pagamento: {card1.data_limite.ToShortDateString()}");
+
+            Console.WriteLine($"\nNome do cartão 2: {card2.nome}");
+            Console.WriteLine($"Número: {card2.numero}");
+            Console.WriteLine($"Data limite de pagamento: {card2.data_limite.ToShortDateString()}");
         }
     }
 }
