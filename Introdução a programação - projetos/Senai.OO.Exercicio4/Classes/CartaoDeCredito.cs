d654817a9f0d6f23369946271549fd780563fe34
Ariel Paixão Dos Santos <33747821839@info.local>
2018-10-01 13:39:41 -0300

    Exercícios 2-4

---
+++ Introdução a programação - projetos/Senai.OO.Exercicio4/Classes/CartaoDeCredito.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Senai.OO.Exercicio4.Classes
+{
+    public class CartaoDeCredito
+    {
+        public string nome;
+        public DateTime data_limite;
+        public int numero;
+    }
+}
---
+++ Introdução a programação - projetos/Senai.OO.Exercicio4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Senai.OO.Exercicio4.Classes;
+
+namespace Senai.OO.Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CartaoDeCredito card1 = new CartaoDeCredito();
+            CartaoDeCredito card2 = new CartaoDeCredito();
+
+            Console.WriteLine("Cartões de Crédito\n");
+            
+            #region card1
+            Console.WriteLine("Insira o nome do primeiro cartão:");
+            card1.nome = Console.ReadLine();
+
+            Console.WriteLine("Insira o número do cartão:");
+            card1.numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Insira a data limite de pagamento do cartão de crédito:");
+            card1.data_limite = Convert.ToDateTime(Console.ReadLine());
+            #endregion
+
+            #region card2
+            Console.WriteLine("Insira o nome do primeiro cartão:");
+            card1.nome = Console.ReadLine();
+
+            Console.WriteLine("Insira o número do cartão:");
+            card1.numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Insira a data limite de pagamento do cartão de crédito:");
+            card1.data_limite = Convert.ToDateTime(Console.ReadLine());
+            #endregion
+
+            //CONTINUAR
+        }
+    }
+}
