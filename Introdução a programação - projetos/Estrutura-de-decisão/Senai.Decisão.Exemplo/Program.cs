2d4f7b744f9820d95e6943194ef9a6862e0dd0e0
Ariel Paixão Dos Santos <33747821839@info.local>
2018-10-01 17:33:34 -0300

    estrututa de decisao

---
+++ Introdução a programação - projetos/Estrutura-de-decisão/Senai.Decisão.Exemplo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Senai.Decisão.Exemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Recebendo um número
+            System.Console.WriteLine("Insira um número:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            //Estrutura de decisão (if)
+            if (numero >= 0)
+            {
+                System.Console.WriteLine("Positivo.");
+            } else {
+                System.Console.WriteLine("Negativo.");
+            }
+        }
+    }
+}
---
+++ Introdução a programação - projetos/Variaveis/code dojo prestação/Calculo.Prestacao/classe/Juros.cs
@@ -0,0 +1,19 @@
+namespace Calculo.Prestacao.classe
+{
+    public class Juros
+    {
+        public string nome;
+
+        public float vReal;
+
+        public int qPrestacao;
+
+        public float vPrestacao;
+
+        public float vAcrescimo;
+
+        public float vMensal;
+
+        public float vTotal;
+    }
+}
