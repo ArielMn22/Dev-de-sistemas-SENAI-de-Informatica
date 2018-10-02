a5fd38c49062fef051dabeabc74c4d9eea343969
Ariel Paixão Dos Santos <33747821839@info.local>
2018-10-02 17:29:07 -0300

    dois

---
+++ Introdução a programação - projetos/Estrutura-de-decisão/Senai.Decisão.Exercícios11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Senai.Decisão.Exercícios11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n; //numero
+
+            Console.Clear();
+
+            System.Console.WriteLine("Insira um número:");
+            n = int.Parse(Console.ReadLine());
+
+            if (n%2 == 0)
+            {
+                System.Console.WriteLine($"\n{n} é par.");
+            } else {
+                System.Console.WriteLine($"\n{n} é ímpar.");
+            }
+        }
+    }
+}
