9b1d6400bc616813c5a8514fb073c9951520c38c
Ariel Paixão Dos Santos <33747821839@info.local>
2018-10-18 14:06:49 -0300

    Exercícios com Array de Objetos

---
+++ Introdução a programação - projetos/Array/Senai.Array.Exercício2.CadastroUsuários/Classes/Usuario.cs
@@ -0,0 +1,18 @@
+namespace Senai.Array.Exercício2.CadastroUsuários.Classes
+{
+    /// <summary>
+    /// Classe criada para cadastrar usuários, com 'Nome', 'Idade', 'Endereco'.
+    /// </summary>
+    public class Usuario
+    {
+        #region Propriedades
+        public string Nome {get; set;}
+        public int Idade {get; set;}
+        public string Endereco {get; set;}
+        #endregion
+
+        #region Metodos
+        //Não foram criados metodos para mostrar as informações para manter a função multiplataforma da classe.
+        #endregion
+    }
+}
