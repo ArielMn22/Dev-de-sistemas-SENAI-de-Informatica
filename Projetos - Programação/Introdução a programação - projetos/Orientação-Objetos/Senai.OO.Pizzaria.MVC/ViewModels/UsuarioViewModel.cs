25c21f0960d92a161b83192992741920569462dc
ArielMn22 <arielpaixao10@gmail.com>
2018-10-26 11:35:19 -0300

    Adicionando Diretório Renomeado

---
+++ Projetos - Programação/Introdução a programação - projetos/Orientação-Objetos/Senai.OO.Pizzaria.MVC/ViewModels/UsuarioViewModel.cs
@@ -0,0 +1,17 @@
+namespace Senai.OO.Pizzaria.MVC.ViewModels
+{
+    /// <summary>
+    /// Classe responsável pelas ações do usuário
+    /// </summary>
+    public class UsuarioViewModel : BaseViewModel
+    {
+        /// <summary>
+        /// Email do usuário.
+        /// </summary>
+        public string Email;
+        /// <summary>
+        /// Senha do usuário.
+        /// </summary>
+        public string Senha;
+    }
+}
