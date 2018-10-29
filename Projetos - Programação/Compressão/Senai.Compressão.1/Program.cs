abd7563ede4da6fbd9b88cdf4c873b444446beab
ArielMn22 <arielpaixao10@gmail.com>
2018-10-29 19:25:51 -0300

    Corrigindo erros

--- Projetos - Programação/Compressão/Senai.Compressão.1/Program.cs
+++ Projetos - Programação/Compressão/Senai.Compressão.1/Program.cs
@@ -15,6 +15,7 @@ namespace Senai.Compressão._1 {
                 System.Console.WriteLine ("========================================================");
                 System.Console.WriteLine ("1 - Compactar");
                 System.Console.WriteLine ("2 - Descompactar");
+                System.Console.WriteLine ("0 - Sair");
 
                 System.Console.Write ("Insira uma opção: ");
                 opcao = Console.ReadLine ();
@@ -26,11 +27,12 @@ namespace Senai.Compressão._1 {
                     case "1":
                         System.Console.WriteLine ("Informe o caminho/nome da pasta que deseja compactar:");
                         directoryName = Console.ReadLine ();
-                        // directoryName = Path.GetFullPath (directoryName);
+                        //directoryName = Path.GetFullPath (directoryName);
+                        //directoryName += Path.DirectorySeparatorChar;
 
                         System.Console.WriteLine ("Informe o caminho/nome do arquivo após a compactação:");
                         fileDestino = Console.ReadLine ();
-                        // fileDestino = Path.GetFullPath (fileDestino);
+                        //fileDestino = Path.GetFullPath (fileDestino);
 
                         if (!Directory.Exists (directoryName))
                         {
@@ -38,20 +40,21 @@ namespace Senai.Compressão._1 {
                         } else if (File.Exists (fileDestino)) {
                             System.Console.WriteLine ("O arquivo já foi descompactado...");
                         } else {
-                            ZipFile.ExtractToDirectory (directoryName, fileDestino);
+                            ZipFile.CreateFromDirectory (directoryName, fileDestino);
                         }
 
+                        System.Console.WriteLine("\nAperte [QUALQUER] tecla para continuar...");
                         Console.ReadKey();
 
                         break;
                     case "2":
                         System.Console.WriteLine ("Informe o caminho do arquivo .zip que deseja descompactar:");
                         directoryName = Console.ReadLine ();
-                        // directoryName = Path.GetFullPath (directoryName);
+                        //directoryName = Path.GetFullPath (directoryName);
 
                         System.Console.WriteLine ("Informe o nome do diretório junto com seu caminho:");
                         fileDestino = Console.ReadLine ();
-                        // fileDestino = Path.GetFullPath (fileDestino);
+                        //fileDestino = Path.GetFullPath (fileDestino);
 
                         if (!File.Exists (directoryName))
                         {
@@ -62,9 +65,18 @@ namespace Senai.Compressão._1 {
                             ZipFile.ExtractToDirectory (directoryName, fileDestino);
                         }
                         
+                        System.Console.WriteLine("\nAperte [QUALQUER] tecla para continuar...");
                         Console.ReadKey ();
 
                         break;
+
+                    case "0":
+                        Console.Clear();
+                        
+                        System.Console.WriteLine("Tchau...");
+
+                        break;
+
                     default:
                         System.Console.WriteLine ("Insira uma opção válida!!");
                         break;
