df6f077cef9e6f8fe485cdf55e1379d3f0dc28a3
Ariel Paixão Dos Santos <33747821839@info.local>
2018-10-29 17:23:32 -0300

    Compressão

--- Projetos - Programação/Compressão/Senai.Compressão.1/Program.cs
+++ Projetos - Programação/Compressão/Senai.Compressão.1/Program.cs
@@ -1,36 +1,73 @@
 ﻿using System;
-// using System.IO;
+using System.IO;
 using System.IO.Compression;
 
 namespace Senai.Compressão._1 {
     class Program {
         static void Main (string[] args) {
             string opcao;
+
             do {
+                Console.Clear ();
+
                 System.Console.WriteLine ("========================================================");
                 System.Console.WriteLine ("                     Compactação");
                 System.Console.WriteLine ("========================================================");
                 System.Console.WriteLine ("1 - Compactar");
                 System.Console.WriteLine ("2 - Descompactar");
 
+                System.Console.Write ("Insira uma opção: ");
                 opcao = Console.ReadLine ();
+
+                string directoryName = null;
+                string fileDestino = null;
 
                 switch (opcao) {
                     case "1":
-                        string diretorioAlvo = @"PastaParaCompactar";
-                        string arquivoDestino = @"PastaCompactada.zip";
+                        System.Console.WriteLine ("Informe o caminho/nome da pasta que deseja compactar:");
+                        directoryName = Console.ReadLine ();
+                        // directoryName = Path.GetFullPath (directoryName);
 
-                        ZipFile.CreateFromDirectory (diretorioAlvo, arquivoDestino);
+                        System.Console.WriteLine ("Informe o caminho/nome do arquivo após a compactação:");
+                        fileDestino = Console.ReadLine ();
+                        // fileDestino = Path.GetFullPath (fileDestino);
+
+                        if (!Directory.Exists (directoryName))
+                        {
+                            System.Console.WriteLine("Diretório alvo não encontrado...");
+                        } else if (File.Exists (fileDestino)) {
+                            System.Console.WriteLine ("O arquivo já foi descompactado...");
+                        } else {
+                            ZipFile.ExtractToDirectory (directoryName, fileDestino);
+                        }
+
+                        Console.ReadKey();
+
                         break;
                     case "2":
-                        string arquivoAlvo = @"PastaCompactada.zip";
-                        string diretorioDestino = @"PastaExtraida";
+                        System.Console.WriteLine ("Informe o caminho do arquivo .zip que deseja descompactar:");
+                        directoryName = Console.ReadLine ();
+                        // directoryName = Path.GetFullPath (directoryName);
 
-                        ZipFile.ExtractToDirectory(arquivoAlvo, diretorioDestino);
+                        System.Console.WriteLine ("Informe o nome do diretório junto com seu caminho:");
+                        fileDestino = Console.ReadLine ();
+                        // fileDestino = Path.GetFullPath (fileDestino);
+
+                        if (!File.Exists (directoryName))
+                        {
+                            System.Console.WriteLine("Diretório alvo não encontrado...");
+                        } else if (Directory.Exists (fileDestino)) {
+                            System.Console.WriteLine ("O arquivo já foi descompactado...");
+                        } else {
+                            ZipFile.ExtractToDirectory (directoryName, fileDestino);
+                        }
+                        
+                        Console.ReadKey ();
+
                         break;
                     default:
-                        System.Console.WriteLine("Insira uma opção válida!!");
-                    break;
+                        System.Console.WriteLine ("Insira uma opção válida!!");
+                        break;
                 }
             } while (opcao != "0");
 
