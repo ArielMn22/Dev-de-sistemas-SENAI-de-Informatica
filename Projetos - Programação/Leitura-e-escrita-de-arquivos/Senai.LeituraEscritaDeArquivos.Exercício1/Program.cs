3adc6bccba25c9a60e8bec69bc8ee16467215cd4
Ariel Paixão Dos Santos <33747821839@info.local>
2018-10-31 17:32:35 -0300

    Adicionando arquivos TechTalk

---
+++ Projetos - Programação/Leitura-e-escrita-de-arquivos/Senai.LeituraEscritaDeArquivos.Exercício1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+
+namespace Senai.LeituraEscritaDeArquivos.Exercício1 {
+    class Program {
+        static void Main (string[] args) {
+            int opcao;
+
+            do {
+                System.Console.WriteLine ("========================================");
+                System.Console.WriteLine ("               MENU");
+                System.Console.WriteLine ("========================================");
+                System.Console.WriteLine ("1 - Ler");
+                System.Console.WriteLine ("2 - Escrever");
+
+                System.Console.WriteLine ("Insira uma opção: ");
+                opcao = int.Parse (Console.ReadLine ());
+
+                switch (opcao) {
+                    case 1:
+                        {
+                            System.Console.WriteLine ("Insira o nome do arquivo que deseja ler:");
+                            string arquivoLer = Console.ReadLine ();
+
+                            if (File.Exists (arquivoLer)) {
+                                string linha;
+                                using (StreamReader ler = new StreamReader (arquivoLer)) {
+                                    while ((linha = ler.ReadLine ()) != null) {
+                                        System.Console.WriteLine (linha);
+                                    }
+                                }
+                            }
+                            break;
+                        }
+                    case 2:
+                        {
+                            System.Console.WriteLine ("Insira o nome do arquivo:");
+                            string arquivo = Console.ReadLine ();
+
+                            System.Console.WriteLine ("O que deseja escrever?");
+                            string mensagem = Console.ReadLine();
+                            
+                            using (StreamWriter escrever = new StreamWriter (arquivo)) {
+                                escrever.WriteLine (mensagem);
+                            }
+
+                            // Mostrando
+
+                            System.Console.WriteLine("Aperte [QUALQUER] tecla para mostrar o arquivo");
+                            Console.ReadKey();
+
+                            string linha;
+                                using (StreamReader ler = new StreamReader (arquivo)) {
+                                    while ((linha = ler.ReadLine ()) != null) {
+                                        System.Console.WriteLine (linha);
+                                    }
+                                }
+                                break;
+                        }
+                    default:
+                    break;
+                }
+            } while (opcao != 0);
+        }
+    }
+}
---
+++ Projetos - Programação/Leitura-e-escrita-de-arquivos/Senai.LeituraEscritaDeArquivos.Primeiro/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO; // abilita input e output para o usuario stream 
+
+namespace Senai.Leitura.Escrita
+{
+    class Program
+    {
+        static int[] numeros = new int[5]; // vetor para guardar os numeros randomecos 
+        static string Linha; 
+        static Random rand = new Random(); // para gerar numeros aleatoria 
+        static void Main(string[] args)
+        {
+           if (File.Exists("string.txt")) // verifica se o arquivo existe 
+           {
+               System.Console.WriteLine("\nO arquivo ja existe");
+               using(StreamReader ler = new StreamReader("string.txt")){ // inicia o a leitura do arquivo 
+                   while ((Linha = ler.ReadLine()) != null) // armazena a leitura dentro da string linha 
+                   {
+                       System.Console.WriteLine($"{Linha}");
+                   }
+               }
+           }
+            
+               else{ 
+                   for (int i = 0; i < 5; i++) // armazena dentro do array os numeros randomicos 
+                   {
+                       numeros[i] = rand.Next(1000);
+                   }
+                   using(StreamWriter Escrever = new StreamWriter("string.txt")){ // inicia o processo de escrita no arquivo se o arquivo nao exite ele cria 
+                       for (int i = 0; i < 5; i++)          { // escreve o array de numeros dentro do arquivo de texto 
+                           Escrever.WriteLine(numeros[i]);
+                       }
+                   }
+
+                   using(StreamReader ler =  new StreamReader("string.txt")){ // le o qu esta escrito em cima no arquivo de texto 
+                       string Linha;
+                       while ((Linha = ler.ReadLine()) != null){
+                           System.Console.WriteLine($"{Linha}");
+                       }
+                       
+                   }
+               }
+           }
+          
+        }
+    }
+
+
